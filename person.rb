94eb65a48e717280f41b6a803698d741636a731c
BHDHakim <hakim.bhd@gmail.com>
2022-08-22 16:49:25 +0300

    Update Person calss

--- person.rb
+++ person.rb
@@ -2,9 +2,9 @@ class Person
   attr_accessor :name, :age
   attr_reader :id
 
-  private :is_of_age
+  private :of_age
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name: 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @age = age
     @name = name
