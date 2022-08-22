cf6da7225112c1921d21b8ad2cf7194f7afaf831
BHDHakim <hakim.bhd@gmail.com>
2022-08-22 18:36:32 +0300

    Add name decorators

--- person.rb
+++ person.rb
@@ -1,10 +1,12 @@
-class Person
+require_relative 'decorators'
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
-  private :of_age
-
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
+
     @id = Random.rand(1..1000)
     @age = age
     @name = name
@@ -18,4 +20,19 @@ class Person
   def can_use_services?
     of_age? || @parent_permission
   end
+
+  def correct_name
+    @name
+  end
+
+  private :of_age?
 end
+
+person = Person.new(22, 'maximilianus')
+person.correct_name
+
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
